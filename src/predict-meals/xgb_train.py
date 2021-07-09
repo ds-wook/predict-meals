50d4f2fe2b7bf8b327b32d22ea2015eb56fa8a9b
ds-wook <leewook94@naver.com>
2021-07-09 18:46:34 +0900

    Update Code!

--- src/predict-meals/xgb_train.py
+++ src/predict-meals/xgb_train.py
@@ -27,7 +27,17 @@ def _main(args: argparse.Namespace):
     y_lunch = train["중식계"]
     X_test = test[["월", "일", "요일", "식사가능자수", "본사출장자수", "본사시간외근무명령서승인건수"]]
 
-    lunch_params = pd.read_pickle()
+    lunch_params = {
+        "lambda": 0.014695381889341316,
+        "alpha": 7.411440351007072,
+        "subsample": 0.8,
+        "learning_rate": 0.02,
+        "max_depth": 5,
+        "min_child_weight": 1,
+    }
+    lunch_params["n_estimators"] = 10000
+    lunch_params["random_state"] = 42
+
     lunch_model = XGBRegressor(**lunch_params)
     lunch_model.fit(X_lunch, y_lunch)
 
@@ -36,7 +46,18 @@ def _main(args: argparse.Namespace):
     X_dinner = train[["월", "일", "요일(석식)", "식사가능자수", "본사출장자수", "본사시간외근무명령서승인건수"]]
     y_dinner = train["석식계"]
 
-    dinner_params = pd.read_pickle()
+    dinner_params = {
+        "lambda": 0.014750443224224807,
+        "alpha": 0.0010890646914942937,
+        "colsample_bytree": 1.0,
+        "subsample": 0.6,
+        "learning_rate": 0.012,
+        "max_depth": 7,
+        "min_child_weight": 2,
+    }
+    dinner_params["n_estimators"] = 10000
+    dinner_params["random_state"] = 42
+
     dinner_model = XGBRegressor(**dinner_params)
     dinner_model.fit(X_dinner, y_dinner)
     dinner_preds = dinner_model.predict(X_test)
