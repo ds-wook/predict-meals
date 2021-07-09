509a5d0b7ed3c061feff5de46e49c3541f7dd5db
ds-wook <leewook94@naver.com>
2021-07-09 18:43:27 +0900

    Update!

--- notebook/eda.py
+++ notebook/eda.py
@@ -1,10 +1,13 @@
 # %%
+import warnings
+
 import matplotlib.font_manager as fm
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 
+warnings.filterwarnings("ignore")
 path_gothic = "/usr/share/fonts/truetype/nanum/NanumGothic.ttf"
 fontprop = fm.FontProperties(fname=path_gothic, size=20)
 
@@ -19,18 +22,18 @@ train.head()
 train.info()
 # %%
 train.columns = [
-    "date",
-    "weekday",
-    "employees",
-    "dayoff",
-    "bustrip",
-    "overtime",
-    "remote",
-    "breakfast",
-    "lunch",
-    "dinner",
-    "target_lunch",
-    "target_dinner",
+    "date",  # 일자
+    "weekday",  # 요일
+    "employees",  # 본사정원수
+    "dayoff",  # 본사휴가자수
+    "bustrip",  # 본사출장자수
+    "overtime",  # 본사외무령승건
+    "remote",  # 재택근무자수
+    "breakfast",  # 조식메뉴
+    "lunch",  # 중식메뉴
+    "dinner",  # 석식메뉴
+    "target_lunch",  # 중식계
+    "target_dinner",  # 석식계
 ]
 train.head()
 # %%
@@ -38,22 +41,129 @@ train["date"] = pd.to_datetime(train["date"])
 train.info()
 
 # %%
-train.plot(x="date", y="target_lunch", figsize=(40, 5))
-plt.show()
+test.head()
+# %%
+test.columns = [
+    "date",  # 일자
+    "weekday",  # 요일
+    "employees",  # 본사정원수
+    "dayoff",  # 본사휴가자수
+    "bustrip",  # 본사출장자수
+    "overtime",  # 본사외무령승건
+    "remote",  # 재택근무자수
+    "breakfast",  # 조식메뉴
+    "lunch",  # 중식메뉴
+    "dinner",  # 석식메뉴
+]
+test.head()
+# %%
+test["date"] = pd.to_datetime(test["date"])
+test.info()
+# %%
+
+
+def to_datetime(df, date):
+    df["date"] = pd.to_datetime(df[date])
+    df["dow"] = pd.to_datetime(df[date]).dt.weekday + 1
+
+
+to_datetime(train, "date")
+to_datetime(test, "date")
+
 
 # %%
-train.query("date >= 2017")
+def make_lunch(df):
+    lunch = []
+    for day in range(len(df)):
+        tmp = df.iloc[day, 8].split(" ")  # 공백으로 문자열 구분
+        tmp = " ".join(tmp).split()  # 빈 원소 삭제
+
+        search = "("  # 원산지 정보는 삭제
+        for menu in tmp:
+            if search in menu:
+                tmp.remove(menu)
+
+        lunch.append(tmp)
+    return lunch
+
+
 # %%
-train.loc[train["dinner"] == "*"]
+
+train_lunch = make_lunch(train)
+test_lunch = make_lunch(test)
+
 # %%
-train.loc[train["date"] == "2019-08-28"]["dinner"].values
+
+
+def make_lunch_cols(lunch, df):
+    # lunch train data에 메뉴명별 칼럼 만들기 (밥, 국, 반찬1-3)
+    bob = []
+    gook = []
+    banchan1 = []
+    banchan2 = []
+    banchan3 = []
+    kimchi = []
+    side = []
+    for i, day_menu in enumerate(lunch):
+        bob_tmp = day_menu[0]
+        bob.append(bob_tmp)
+        gook_tmp = day_menu[1]
+        gook.append(gook_tmp)
+        banchan1_tmp = day_menu[2]
+        banchan1.append(banchan1_tmp)
+        banchan2_tmp = day_menu[3]
+        banchan2.append(banchan2_tmp)
+        banchan3_tmp = day_menu[4]
+        banchan3.append(banchan3_tmp)
+
+        if i < 1067:
+            kimchi_tmp = day_menu[-1]
+            kimchi.append(kimchi_tmp)
+            side_tmp = day_menu[-2]
+            side.append(side_tmp)
+        else:
+            kimchi_tmp = day_menu[-2]
+            kimchi.append(kimchi_tmp)
+            side_tmp = day_menu[-1]
+            side.append(side_tmp)
+    df_ln = df[
+        [
+            "date",
+            "weekday",
+            "dow",
+            "employees",
+            "dayoff",
+            "bustrip",
+            "overtime",
+            "remote",
+            "lunch",
+        ]
+    ]
+
+    df_ln["bob"] = bob
+    df_ln["gook"] = gook
+    df_ln["banchan1"] = banchan1
+    df_ln["banchan2"] = banchan2
+    df_ln["banchan3"] = banchan3
+    df_ln["kimchi"] = kimchi
+    df_ln["side"] = side
+    df_ln.head()
+    return df_ln
+
+
 # %%
-train["dinner"] = train["dinner"].str.strip()
-train.loc[train["dinner"] == "*"]
+train_ln = make_lunch_cols(train_lunch, train)
+train_ln["target_lunch"] = train["target_lunch"]
+test_ln = make_lunch_cols(test_lunch, test)
+
 # %%
-train.loc[train["target_dinner"] == 0]
+train_ln.info()
 # %%
-train["weekday"].unique()
+test_ln.info()
+
 # %%
-train.loc[train["date"] == "2019-01-02"]
+
+train_ln["weekday"] = train_ln["weekday"].map({"월": 0, "화": 1, "수": 2, "목": 3, "금": 4})
+test_ln["weekday"] = test_ln["weekday"].map({"월": 0, "화": 1, "수": 2, "목": 3, "금": 4})
+
 # %%
---
+++ src/predict-meals/xgb_train.py
@@ -0,0 +1,53 @@
+import argparse
+
+import pandas as pd
+from xgboost import XGBRegressor
+
+from data.dataset import load_dataset
+
+
+def define_argparser():
+    parse = argparse.ArgumentParser("Training!")
+    parse.add_argument(
+        "--submit", type=str, help="Input data save path", default="../../submission/"
+    )
+    parse.add_argument(
+        "--path", type=str, default="../../input/predict-credit-card-delinquency/"
+    )
+    parse.add_argument("--file", type=str, help="Input file name", default="model.csv")
+
+    args = parse.parse_args()
+    return args
+
+
+def _main(args: argparse.Namespace):
+    path = args.path
+    train, test = load_dataset(path)
+    X_lunch = train[["월", "일", "요일", "식사가능자수", "본사출장자수", "본사시간외근무명령서승인건수"]]
+    y_lunch = train["중식계"]
+    X_test = test[["월", "일", "요일", "식사가능자수", "본사출장자수", "본사시간외근무명령서승인건수"]]
+
+    lunch_params = pd.read_pickle()
+    lunch_model = XGBRegressor(**lunch_params)
+    lunch_model.fit(X_lunch, y_lunch)
+
+    lunch_preds = lunch_model.predict(X_test)
+
+    X_dinner = train[["월", "일", "요일(석식)", "식사가능자수", "본사출장자수", "본사시간외근무명령서승인건수"]]
+    y_dinner = train["석식계"]
+
+    dinner_params = pd.read_pickle()
+    dinner_model = XGBRegressor(**dinner_params)
+    dinner_model.fit(X_dinner, y_dinner)
+    dinner_preds = dinner_model.predict(X_test)
+
+    submission = pd.read_csv(path + "sample_submission.csv")
+    submission["중식계"] = lunch_preds
+    submission["석식계"] = dinner_preds
+
+    submission.to_csv(args.path + args.file, index=False)
+
+
+if __name__ == "__main__":
+    args = define_argparser()
+    _main(args)
